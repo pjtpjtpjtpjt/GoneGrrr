19b78c21b291413617ed1199a8daf3314830468d
Paul <paul.jo.thompson@gmail.com>
2016-04-25 05:42:14 -0700

    add to firebase

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,11 +1,18 @@
 var app = angular.module('GoneGrrrapp', ['firebase','ui.router']);
-    app.controller('GrrrController', function($scope, $firebaseArray) {
-        var ref = new Firebase('https://blistering-fire-9917.firebaseio.com');
+    app.controller('GrrrController', function($scope, $firebaseArray) {                                 
+        
+              
+       var ref = new Firebase('https://blistering-fire-9917.firebaseio.com');
         $scope.tasks = $firebaseArray(ref.child('tasks'));
+        
         $scope.addTask = function(){
             $scope.tasks.$add({
                 taskTitle: $scope.firstTask,
                 taskActive: true
             });
         }
+        
+       
+        
+        
  });
