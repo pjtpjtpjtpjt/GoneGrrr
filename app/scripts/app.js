8796ed67ae690ca109066aa70f860b919b618c39
Paul <paul.jo.thompson@gmail.com>
2016-04-25 08:24:28 -0700

    ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,29 @@
+(function() {
+     function config($stateProvider, $locationProvider) {
+         $locationProvider
+             .html5Mode({
+                 enabled: true,
+                 requireBase: false
+             });
+
+         $stateProvider
+         .state('home', {
+             url: '/',
+             templateUrl: '/templates/home.html',
+             controller: 'GrrrController'
+         });  
+     }
+
 var app = angular.module('GoneGrrrapp', ['firebase','ui.router']);
+    app.config(config);
     app.controller('GrrrController', function($scope, $firebaseArray) {
         var ref = new Firebase('https://blistering-fire-9917.firebaseio.com');
         $scope.tasks = $firebaseArray(ref.child('tasks'));
         $scope.addTask = function(){
+            console.log($scope.firstTask);
             $scope.tasks.$add({
                 taskTitle: $scope.firstTask,
                 taskActive: true
             });
         }
- });
+ });})();
