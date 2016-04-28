81bac0c4ce32d728ee9c883987e4cf5542a3a1f7
Paul <paul.jo.thompson@gmail.com>
2016-04-28 08:30:05 -0700

    hide old-4 and view past-5

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,7 +11,15 @@
              url: '/',
              templateUrl: '/templates/home.html',
              controller: 'GrrrController'
-         });  
+         });
+         
+         $stateProvider
+         .state('task-history', {
+             url: '/',
+             templateUrl: '/templates/task-history.html',
+             controller: 'GrrrController'
+         }); 
+         
      }
 
 var app = angular.module('GoneGrrrapp', ['firebase','ui.router']);
@@ -19,11 +27,47 @@ var app = angular.module('GoneGrrrapp', ['firebase','ui.router']);
     app.controller('GrrrController', function($scope, $firebaseArray) {
         var ref = new Firebase('https://blistering-fire-9917.firebaseio.com');
         $scope.tasks = $firebaseArray(ref.child('tasks'));
-        $scope.addTask = function(){
-            console.log($scope.firstTask);
-            $scope.tasks.$add({
-                taskTitle: $scope.firstTask,
-                taskActive: true
-            });
+        setInterval(function () {
+        var timeNow = Date.now()
+            for(i =0; i < $scope.tasks.length; i++){
+                if ($scope.tasks[i].taskCompleted === true){
+                    return
+                };
+                
+                var timeDiff = (timeNow-$scope.tasks[i].taskCreatedActual)
+                var expiredTime = (timeDiff/1000)/60;
+                    if(expiredTime > 1){
+                        $scope.tasks[i].taskActive = false;
+                        $scope.tasks.$save($scope.tasks[i]);
+                    } 
+                }
+        }, 10000);
+
+        $scope.hideCriteriaTest = function(){
+            if(this.task.taskCompleted === true || !this.task.taskActive){
+            return true;
+            };
         }
+        
+        $scope.updateTaskCompleted = function (){
+            if(this.task.taskCompleted === true){
+                this.task.taskCompleted = false
+                $scope.tasks.$save(this.task);
+            } else {
+                this.task.taskCompleted = true;
+                $scope.tasks.$save(this.task);
+            }
+        }
+        
+        $scope.addTask = function(){
+                $scope.createdTime = Date.now()
+                $scope.createdTimeString = new Date().toLocaleString()
+                $scope.tasks.$add({
+                    taskTitle: $scope.firstTask,
+                    taskActive: true,
+                    taskCompleted: false,
+                    taskCreatedActual: $scope.createdTime,
+                    taskCreated: $scope.createdTimeString,
+                });
+            }
  });})();
